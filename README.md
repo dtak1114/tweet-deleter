aee49b66adfb973ea34b9f9ba4a0fde04aec2b04
Daisuke <d.takahashi1811@gmail.com>
2022-09-04 15:05:38 +0900

    Add: created

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# tweet-deleter
-deleted twees periodically
+# About
+
+sets up AWS event + lambda function to periodically clean up your tweets posted before specified period.
+
+# Prerequisites
+
+- you have an AWS account and have done setup successfully with `aws configure`.
+- you have your Twitter OAuth credential suites from [here](https://developer.twitter.com/en/portal/dashboard).
+  - you will need to issue these 4 key/secrets to issue requests to Twitter.
+    1. consumer key (`TW_CONSUMER_KEY`)
+    2. consumer secret (`TW_CONSUMER_SECRET`)
+    3. access token key (`TW_ACCESS_TOKEN_KEY`)
+    4. access token secret (`TW_ACCESS_TOKEN_SECRET`)
+
+# Deploy
+
+1. install chalice dependency.
+
+```sh
+pip install -r requirements-dev.txt
+```
+
+2. replace credentials in `app.py`. 
+
+3. deploy.
+
+```sh
+chalice deploy
+```
+
+# Configuration & Limitation
+
+- Due to Twitter's [rate limit](https://developer.twitter.com/ja/docs/twitter-api/rate-limits), it removes upto `50 tweets/15min` == `200 tweets/1hour`. Just hold on tight..
+- By default, tweets posted before 6 month from the moment of execution will be the target of clean up.
+ - you can tweak by editing `app.py#REMOVE_BEFORE` if you'd like.
---
+++ app.py
@@ -0,0 +1,46 @@
+from chalice import Chalice, Rate
+import tweepy
+import os
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+# by default, tweets before 6 months from now are removed.
+REMOVE_BEFORE = datetime.now() -relativedelta(months=6)
+
+app = Chalice(app_name='tweet-deleter')
+
+client = tweepy.Client(
+    consumer_key=os.environ.get('TW_CONSUMER_KEY'),
+    consumer_secret=os.environ.get('TW_CONSUMER_SECRET'),
+    access_token=os.environ.get('TW_ACCESS_TOKEN_KEY'),
+    access_token_secret=os.environ.get('TW_ACCESS_TOKEN_SECRET'),
+    )
+
+
+def get_my_user_id():
+    u = client.get_me()
+    return u.data['id']
+
+def delete_tweets_before(myid, dt, next_token=None):
+    res = client.get_users_tweets(
+        myid,
+        pagination_token=next_token,
+        user_auth=True,
+        end_time=dt,
+        max_results=50)
+    count = 0
+    for t in res.data:
+        client.delete_tweet(t['id'])
+        print(t)
+        count += 1
+    return count
+
+# Automatically runs every day
+@app.schedule(Rate(15, unit=Rate.MINUTES))
+def delete_tweets(event):
+    myid = get_my_user_id()
+    deleted = delete_tweets_before(myid,REMOVE_BEFORE)
+    return deleted
+
+if __name__ == '__main__':
+    delete_tweets()
